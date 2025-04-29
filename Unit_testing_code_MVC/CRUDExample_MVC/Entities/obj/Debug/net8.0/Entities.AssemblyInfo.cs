44e51458dd3dcb8b643d556090d03c6488819dce
SureshBabu <sureshbabuj@student.tce.edu>
2025-04-29 23:55:22 +0530

    CRUD MVC

--- Unit_testing_code_MVC/CRUDExample_MVC/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Unit_testing_code_MVC/CRUDExample_MVC/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db812171a9ef0516ed08c60ff0f6b10f58fc5b78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c89a5349e0da6278403318f5c3015f92310604f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
